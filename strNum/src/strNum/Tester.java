f159cf9f12d767f9c70203703453bd1f701c6316
roish <roish@roish-PC>
2013-12-08 14:23:15 +0200

    the test calculate1 sends to function error input and fail

--- strNum/src/strNum/Tester.java
+++ strNum/src/strNum/Tester.java
@@ -1,16 +1,29 @@
 package strNum;
 
 import static org.junit.Assert.*;
+import junit.framework.Assert;
 
 import org.junit.Test;
 
 public class Tester {
-
 	@Test
-	public void test() {
-		String str="roi";
+	public void calculat0() 
+	{
 		strNum s = new strNum();
-		 s.Add(str);
+		s.Add(" ");
 	}
-
+	
+	
+	@Test
+	public void calculat1()
+	{
+		String str="1x2ccccsdfsdfsdfsdf3";
+		strNum s = new strNum();
+		System.out.println(s.Add(str1));
+		
+	}
+	
+	
 }
+
+
--- strNum/src/strNum/strNum.java
+++ strNum/src/strNum/strNum.java
@@ -1,12 +1,13 @@
 package strNum;
 
 public class strNum {
-	
-	public int Add(String str)
-	{
+	public int Add(String numbers){
+		int j=0;
+		for (int i = 0; i < numbers.length(); i++) 
+			if(numbers.charAt(i)>47 && numbers.charAt(i)<58 )
+			 j +=(int)numbers.charAt(i)-48;
 		
-		return 0;
-		
+		return j;
 	}
 
 }
